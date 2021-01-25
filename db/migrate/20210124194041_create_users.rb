b7e512a36a570d9bacbc72df592adf0614b53dd4
Carson Cole <carson.cole@protonmail.com>
2021-01-24 16:32:00 -0800

    fixed precision/scale

--- db/migrate/20210124194041_create_users.rb
+++ db/migrate/20210124194041_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :email, null: false
       t.integer :unit_id
       t.string :screenname, null: false
-      t.decimal :miles_count, default:0, scale: 5, precision: 2
+      t.decimal :miles_count, default:0, scale: 2, precision: 5
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
--- db/migrate/20210124203814_create_treks.rb
+++ db/migrate/20210124203814_create_treks.rb
@@ -3,7 +3,7 @@ class CreateTreks < ActiveRecord::Migration[6.0]
     create_table :treks do |t|
       t.references :user, null: false, foreign_key: true
       t.date :date
-      t.decimal :distance_in_miles, scale: 5, precision: 2, default: 0
+      t.decimal :distance_in_miles, scale: 2, precision: 5, default: 0
 
       t.timestamps
     end
