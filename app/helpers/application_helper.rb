21f42b40ea9e8c0b176c8ed0c848df07f5dd195e
Carson Cole <carson.cole@protonmail.com>
2021-01-27 10:30:28 -0800

    [FIX] Fixed responsive issues with forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def nice_date(date)
     date.strftime('%B %d, %Y') unless date.nil?
   end
+
+  def nice_compact_date(date)
+    date.strftime('%d-%b-%y') unless date.nil?
+  end
 end
