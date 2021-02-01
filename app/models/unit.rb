e61d5dd7e88bd7a130db50cd6899b889506a2737
Carson Cole <carson.cole@protonmail.com>
2021-01-31 18:30:38 -0800

    fix

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,6 +2,6 @@ class Unit < ApplicationRecord
   validates :name, :start_date, :end_date, :miles_goal, presence: true
 
   def active?
-    start_date && start_date < Time.now + 8.hours && end_date + 8.hours > Time.now
+    start_date && start_date < Time.now - 8.hours && end_date - 8.hours > Time.now
   end
 end
